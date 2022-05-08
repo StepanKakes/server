8687181614272dadfded9a869a93b4b3825e67f4
Štěpán Kakeš <91247524+StepanKakes@users.noreply.github.com>
2022-05-08 17:22:32 +0200

    Aktualizovat main.ts, main.py

--- main.py
+++ main.py
@@ -1,33 +1,94 @@
 # hlasovani: List[string] = []
-hlasovani: List[any] = []
-key=False
-def on_button_pressed_b():
-    if key==True:
-        key=False
-    else:
-        key=True
-input.on_button_pressed(Button.B, on_button_pressed_b)
-def on_button_pressed_a():
-    radio.send_value("A", control.device_serial_number())
-input.on_button_pressed(Button.A, on_button_pressed_a)
+A=0
+B=0
+C=0
+D=0
+radio.set_transmit_serial_number(True)
+hlasovani=[{"sn":125638, "value": "D"}]
 
-def on_received_value(name, value):
+#hlasovani.pop()
+key=True
+serial_num=2
+value=65
+pins.touch_set_mode(TouchTarget.P0, TouchTargetMode.CAPACITIVE)
+
+def soucet():
+    global hlasovani,A,B,C,D
+    list=[]
+    for x in hlasovani:
+        list.append(x["value"])
+    A=list.count("A")
+    B=list.count("B")
+    C=list.count("C")
+    D=list.count("D")
+    odpovedi= {"A":A,"B":B,"C":C,"D":D}
+
+    print(odpovedi)
+def on_logo_event_pressed():
+    radio.send_value("hlas", value)
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_event_pressed)
+
+def on_pin_pressed_p0():
+    global hlasovani
+    soucet()
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+
+def on_button_pressed_b():
     global key
     if key==True:
-        values=[name,value]
-        zapocteni(values)
-        console.log_value("hlasovani", hlasovani[0])
+        key=False
+        radio.send_number(0)
+    else:
+        key=True
+        radio.send_number(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_button_pressed_a():
+    global value
+    print(value)
+    if value==68:
+        value=65
+    else:
+        value += 1
+    basic.clear_screen()
+    basic.show_string(String.from_char_code(value))
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_received_number(receivedNumber):
+    global serial_num
+    if receivedNumber==1:
+        key==True
+        basic.clear_screen()
+        basic.show_icon(IconNames.YES)
+    elif receivedNumber==0:
+        key=False
+        basic.clear_screen()
+        basic.show_icon(IconNames.NO)
+    if receivedNumber==control.device_serial_number():
+        print("nvm")
+        basic.clear_screen()
+        basic.show_icon(IconNames.HEART)
+    print(control.device_serial_number())
+radio.on_received_number(on_received_number)
+
+
+def on_received_value(name, value):
+    print(name)
+    global hlasovani,serial_num
+    if name=="hlas":
+        serial_num=radio.received_packet(RadioPacketProperty.SERIAL_NUMBER)
+        key=True
+        radio.send_number(serial_num)
+        console.log_value("serial_num", serial_num)
+        for x in hlasovani:
+            if x["sn"]==serial_num:
+                x["value"]=String.from_char_code(value)
+                key=False
+                break
+        if key==True:
+            hlasovani.push({"sn":serial_num, "value": String.from_char_code(value)})
 radio.on_received_value(on_received_value)
 
 
-def zapocteni(values):
-    global hlasovani
-    key=True
-    for x in range(len(hlasovani)):
-        if hlasovani[x][1]==values[1]:
-            hlasovani[x][0]=values[0]
-            key=False
-            break
-    print(x)
-    if key==True:
-        hlasovani.append(values)
+
+    
--- main.ts
+++ main.ts
@@ -1,44 +1,115 @@
 //  hlasovani: List[string] = []
-let hlasovani : any[] = []
-let key = false
+let A = 0
+let B = 0
+let C = 0
+let D = 0
+radio.setTransmitSerialNumber(true)
+let hlasovani = [ {
+    "sn" : 125638,
+    "value" : "D",
+}
+]
+// hlasovani.pop()
+let key = true
+let serial_num = 2
+let value = 65
+pins.touchSetMode(TouchTarget.P0, TouchTargetMode.Capacitive)
+function soucet() {
+    
+    let list = []
+    for (let x of hlasovani) {
+        list.push(x["value"])
+    }
+    A = _py.py_array_count(list, "A")
+    B = _py.py_array_count(list, "B")
+    C = _py.py_array_count(list, "C")
+    D = _py.py_array_count(list, "D")
+    let odpovedi = {
+        "A" : A,
+        "B" : B,
+        "C" : C,
+        "D" : D,
+    }
+    
+    console.log(odpovedi)
+}
+
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_event_pressed() {
+    radio.sendValue("hlas", value)
+})
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    
+    soucet()
+})
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    let key: boolean;
+    
     if (key == true) {
         key = false
+        radio.sendNumber(0)
     } else {
         key = true
+        radio.sendNumber(1)
     }
     
 })
 input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    radio.sendValue("A", control.deviceSerialNumber())
-})
-radio.onReceivedValue(function on_received_value(name: string, value: number) {
-    let values: string[];
     
-    if (key == true) {
-        values = [name, value]
-        zapocteni(values)
-        console.logValue("hlasovani", hlasovani[0])
+    console.log(value)
+    if (value == 68) {
+        value = 65
+    } else {
+        value += 1
     }
     
+    basic.clearScreen()
+    basic.showString(String.fromCharCode(value))
 })
-function zapocteni(values: any) {
-    let x: number;
+radio.onReceivedNumber(function on_received_number(receivedNumber: number) {
+    let key: boolean;
     
-    let key = true
-    for (x = 0; x < hlasovani.length; x++) {
-        if (hlasovani[x][1] == values[1]) {
-            hlasovani[x][0] = values[0]
-            key = false
-            break
+    if (receivedNumber == 1) {
+        key == true
+        basic.clearScreen()
+        basic.showIcon(IconNames.Yes)
+    } else if (receivedNumber == 0) {
+        key = false
+        basic.clearScreen()
+        basic.showIcon(IconNames.No)
+    }
+    
+    if (receivedNumber == control.deviceSerialNumber()) {
+        console.log("nvm")
+        basic.clearScreen()
+        basic.showIcon(IconNames.Heart)
+    }
+    
+    console.log(control.deviceSerialNumber())
+})
+radio.onReceivedValue(function on_received_value(name: string, value: number) {
+    let key: boolean;
+    console.log(name)
+    
+    if (name == "hlas") {
+        serial_num = radio.receivedPacket(RadioPacketProperty.SerialNumber)
+        key = true
+        radio.sendNumber(serial_num)
+        console.logValue("serial_num", serial_num)
+        for (let x of hlasovani) {
+            if (x["sn"] == serial_num) {
+                x["value"] = String.fromCharCode(value)
+                key = false
+                break
+            }
+            
+        }
+        if (key == true) {
+            hlasovani.push( {
+                "sn" : serial_num,
+                "value" : String.fromCharCode(value),
+            }
+            )
         }
         
     }
-    console.log(x)
-    if (key == true) {
-        hlasovani.push(values)
-    }
     
-}
-
+})
